4a719787ac4871584404403a12a3f7d1b81a0a43
Yash Agarwal <yashagarwaljpr@gmail.com>
2023-07-05 18:17:54 +0530

    fix build failure

--- env.mjs
+++ env.mjs
@@ -1,5 +1,6 @@
-import { createEnv } from "@t3-oss/env-nextjs"
-import { z } from "zod"
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
 
 export const env = createEnv({
   server: {
@@ -8,10 +9,13 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string().min(1),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     IP_ADDRESS_SALT: process.env.IP_ADDRESS_SALT,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   },
 })
