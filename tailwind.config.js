28726fe9c770bd8d7edef4009b2a5e9f2564acaf
Yash Agarwal <yashagarwaljpr@gmail.com>
2023-07-07 13:03:55 +0530

    chore: preparing to move to tailwindcss theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 const disabledCss = {
   "code::before": false,
   "code::after": false,
-  pre: false,
-  code: false,
-  "pre code": false,
 }
 
 /** @type {import('tailwindcss').Config} */
@@ -25,41 +22,72 @@ module.exports = {
     },
     extend: {
       colors: {
-        black: "#0A0A0A",
-        white: "#FFFFFF",
-        primary: {
-          DEFAULT: "#4433FF",
-          300: "#695CFF",
-          400: "#4433FF",
-          500: "#1500FA",
-          600: "#1000C2",
+        text: "hsl(var(--color-text))",
+        muted: "hsl(var(--color-muted))",
+        info: "hsl(var(--color-info))",
+        background: {
+          DEFAULT: "hsl(var(--color-background))",
+          muted: "hsl(var(--color-muted-background))",
+          subtle: "hsl(var(--color-subtle-background))",
         },
-        secondary: {
-          DEFAULT: "#2C0B8E",
-          300: "#2C0B8E",
-          400: "#1C075A",
-          500: "#0C0326",
+        foreground: "hsl(var(--color-foreground))",
+        primary: "hsl(var(--color-primary))",
+        secondary: "hsl(var(--color-secondary))",
+        tertiary: "hsl(var(--color-tertiary))",
+        code: {
+          bg: "hsl(var(--syntax-bg))",
+          text: "hsl(var(--syntax-txt))",
         },
-        tertiary: {
-          DEFAULT: "#E60067",
-          200: "#FF107B",
-          300: "#E60067",
-          400: "#AE004E",
-          500: "#760035",
+        gray: {
+          DEFAULT: "hsl(var(--color-gray-400))",
+          100: "hsl(var(--color-gray-100))",
+          200: "hsl(var(--color-gray-200))",
+          300: "hsl(var(--color-gray-300))",
+          400: "hsl(var(--color-gray-400))",
+          500: "hsl(var(--color-gray-500))",
+          600: "hsl(var(--color-gray-600))",
+          700: "hsl(var(--color-gray-700))",
+          900: "hsl(var(--color-gray-900))",
+          1000: "hsl(var(--color-gray-1000))",
         },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
         mono: ["var(--font-mono)"],
         body: ["var(--font-body)"],
         heading: ["var(--font-body)"],
       },
-      typography: {
+      typography: ({ theme }) => ({
+        article: {
+          css: {
+            "--tw-prose-body": theme("colors.text"),
+            "--tw-prose-headings": theme("colors.tertiary"),
+            "--tw-prose-lead": theme("colors.text"),
+            "--tw-prose-links": theme("colors.primary"),
+            "--tw-prose-bold": theme("colors.text"),
+            "--tw-prose-counters": theme("colors.primary"),
+            "--tw-prose-bullets": theme("colors.primary"),
+            "--tw-prose-hr": theme("colors.gray[300]"),
+            "--tw-prose-quotes": theme("colors.gray[700]"),
+            "--tw-prose-quote-borders": theme("colors.gray[300]"),
+            "--tw-prose-captions": theme("colors.gray[500]"),
+            "--tw-prose-code": theme("colors.code_text"),
+            "--tw-prose-pre-code": theme("colors.code_text"),
+            "--tw-prose-pre-bg": theme("colors.code_bg"),
+            "--tw-prose-th-borders": theme("colors.gray[200]"),
+            "--tw-prose-td-borders": theme("colors.gray[200]"),
+          },
+        },
         DEFAULT: { css: disabledCss },
         sm: { css: disabledCss },
         lg: { css: disabledCss },
         xl: { css: disabledCss },
         "2xl": { css: disabledCss },
-      },
+      }),
     },
   },
   plugins: [require("@tailwindcss/typography")],
