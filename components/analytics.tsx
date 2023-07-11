93fd97bbd44a60520a278bd199b8a0ca3c590cae
Yash Agarwal <yashagarwaljpr@gmail.com>
2023-07-11 10:20:20 +0530

    feat: add goatcounter (#19)

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -4,18 +4,26 @@ import { siteConfig } from "@/config/site"
 import Script from "next/script"
 
 export function Analytics() {
+  const siteUrl = siteConfig.url
   if (
     process.env.NODE_ENV !== "production" ||
-    !siteConfig.url.startsWith("https://yashagarwal.in")
+    !siteConfig.url.startsWith(siteUrl)
   ) {
     return null
   }
 
   return (
-    <Script
-      defer
-      src="https://static.cloudflareinsights.com/beacon.min.js"
-      data-cf-beacon='{"token": "e14f712dcb3f4bb7aab3a2f606159e03"}'
-    />
+    <>
+      <Script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "e14f712dcb3f4bb7aab3a2f606159e03"}'
+      />
+      <Script
+        data-goatcounter="https://yashhere.goatcounter.com/count"
+        async
+        src="//gc.zgo.at/count.js"
+      />
+    </>
   )
 }
