0f61db5922afdba4291b7b0a5f24b29d43813204
Yash Agarwal <yashagarwaljpr@gmail.com>
2023-07-22 00:18:02 +0530

    chore: move tag array shuffling to client

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -1,8 +1,7 @@
 import { Metadata } from "next"
-import Link from "@/components/ui/link"
+import TagCloud from "@/components/tag-clound"
 import { siteConfig } from "@/config/site"
 import { allPosts } from "contentlayer/generated"
-import GithubSlugger from "github-slugger"
 
 export const metadata: Metadata = {
   title: "Tags | Yash Agarwal",
@@ -27,35 +26,12 @@ export default function Page() {
     count: allTags.filter((f) => f === tagName).length,
   }))
 
-  for (let i = tagCounts.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[tagCounts[i], tagCounts[j]] = [tagCounts[j], tagCounts[i]]
-  }
-
   return (
     <>
       <h1 className="leading-extra-tight pb-8 font-heading text-5xl font-bold lg:text-[96px]">
         Tags
       </h1>
-      <div className="text-md mt-2 flex flex-wrap justify-start font-body font-semibold text-gray-600 sm:text-lg">
-        {tagCounts.map((tagEntry) => {
-          const slugger = new GithubSlugger()
-          const tag = tagEntry.tag
-          const tag_slug = tag ? slugger.slug(tag) : undefined
-
-          return (
-            <div className="ml-4 mt-4 inline-flex items-center rounded-full bg-muted px-3 py-1 text-lg font-bold normal-case leading-7">
-              <Link
-                key={tag_slug}
-                href={`/tags/${tag_slug}`}
-                className="mr-2 text-text hover:text-primary"
-              >
-                {tag} ({tagEntry.count})
-              </Link>
-            </div>
-          )
-        })}
-      </div>
+      <TagCloud tagCounts={tagCounts} />
     </>
   )
 }
