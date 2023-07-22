ec913ca2880dd66f8cabee402492b56c2fd0ef01
Yash Agarwal <yashagarwaljpr@gmail.com>
2023-07-22 19:07:04 +0530

    style: increase size of length counter

--- components/ui/section-title.tsx
+++ components/ui/section-title.tsx
@@ -3,7 +3,7 @@ export default function SectionTitle({ title, data }) {
     <h1 className="leading-extra-tight space-x-4 pb-8 font-heading text-5xl font-bold lg:text-[96px]">
       {title}
       {data && data.length !== 0 && (
-        <span className="pl-4 text-sm text-secondary sm:text-lg">
+        <span className="pl-4 text-lg text-secondary sm:text-xl">
           {data.length}
         </span>
       )}
