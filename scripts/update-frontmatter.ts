c62a7aa87e3e9e1afcee5e3ebab31ace7b211b15
Yash Agarwal <yashagarwaljpr@gmail.com>
2023-07-26 08:13:15 +0530

    feat: add auto updateOn field

---
+++ scripts/update-frontmatter.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const fs = require("fs").promises
+const matter = require("gray-matter")
+
+const updateFrontmatter = async () => {
+  const [, , ...mdFilePaths] = process.argv
+
+  mdFilePaths.forEach(async (path) => {
+    const file = matter.read(path)
+    const { data: currentFrontmatter } = file
+
+    if (currentFrontmatter.status === "published") {
+      const updatedFrontmatter = {
+        ...currentFrontmatter,
+        updatedOn: new Date().toISOString(),
+      }
+      file.data = updatedFrontmatter
+      const updatedFileContent = matter.stringify(file)
+      fs.writeFile(path, updatedFileContent)
+    }
+  })
+}
+
+updateFrontmatter()
