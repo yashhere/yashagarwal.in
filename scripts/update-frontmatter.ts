e70f4819b64e84a8383b653ca6060c1e36d6d9f4
Yash Agarwal <yashagarwaljpr@gmail.com>
2023-07-26 08:22:57 +0530

    chore: add Todo task

--- scripts/update-frontmatter.ts
+++ scripts/update-frontmatter.ts
@@ -3,6 +3,15 @@ const fs = require("fs").promises
 const matter = require("gray-matter")
 
 const CUTOFF_DATE = "2023-07-26"
+
+function convertDateToIST(date) {
+  return new Date(
+    (typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {
+      timeZone: "Asia/Kolkata",
+    })
+  )
+}
+
 const updateFrontmatter = async () => {
   const [, , ...mdFilePaths] = process.argv
 
@@ -11,12 +20,15 @@ const updateFrontmatter = async () => {
     const { data: currentFrontmatter } = file
 
     if (currentFrontmatter.status === "published") {
+      // TODO: auto add publishedDate as well
+      // If published is not present, set it to current time, else ignore
       const publishedDate = new Date(currentFrontmatter.published)
       const cutoffDate = new Date(CUTOFF_DATE)
       if (publishedDate >= cutoffDate) {
         const updatedFrontmatter = {
           ...currentFrontmatter,
-          updatedOn: new Date().toISOString(),
+          published: convertDateToIST(publishedDate),
+          updatedOn: convertDateToIST(new Date().toISOString()),
         }
         file.data = updatedFrontmatter
         const updatedFileContent = matter.stringify(file)
