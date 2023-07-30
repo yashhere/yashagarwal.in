d390f167046f4bcfdb886081a4ab49b57dd4ceab
Yash Agarwal <yashagarwaljpr@gmail.com>
2023-07-30 12:43:40 +0530

    chore: don't publish if publish time is less than current time

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -84,6 +84,10 @@ export async function generateMetadata(
 export default async function Page({ params }: Props) {
   const { slug } = params
   const article = await getPartialPost(slug)
+  if (!article) {
+    // TODO: Add 404 here
+    return <></>
+  }
   const MdxContent = getMDXComponent(article.post.body.code)
   const sessionId = getSessionId(slug)
   let [totalLikes, userLikes] = await getLikes(slug, sessionId)
