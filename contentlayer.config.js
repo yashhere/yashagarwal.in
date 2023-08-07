f8d73928bc658e2cfc9a4f3aca240229b6184a78
Yash Agarwal <yashagarwaljpr@gmail.com>
2023-08-07 21:37:15 +0530

    chore: remove the anchor tag from headings

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -25,7 +25,7 @@ export default makeSource({
       [
         rehypeAutolinkHeadings,
         {
-          behaviour: "append",
+          behaviour: "wrap",
           properties: {
             className: ["anchor"],
           },
