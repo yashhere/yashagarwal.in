48e9c36ac7c4c504963a2621c67fceb71f0472a2
Yash Agarwal <yashagarwaljpr@gmail.com>
2024-02-18 11:29:44 +0530

    chore: remove unused env variables

--- lib/server-utils.ts
+++ lib/server-utils.ts
@@ -7,19 +7,3 @@ export function sortNotes(array: Note[]) {
     (a, b) => new Date(b.createdOn).getTime() - new Date(a.createdOn).getTime()
   )
 }
-
-export function getSessionId(slug: string) {
-  const ipAddress = headers().get("x-forwarded-for")
-
-  const currentUserId =
-    // Since a users IP address is part of the sessionId in our database, we
-    // hash it to protect their privacy. By combining it with a salt, we get
-    // get a unique id we can refer to, but we won't know what their ip
-    // address was.
-    createHash("md5")
-      .update(ipAddress + process.env.IP_ADDRESS_SALT!, "utf8")
-      .digest("hex")
-
-  // Identify a specific users interactions with a specific note
-  return slug + "___" + currentUserId
-}
