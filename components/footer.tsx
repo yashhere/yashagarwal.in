3aa117c9bfae5ee8e9c3fc01978aa61e7ae643c6
Yash Agarwal <yashagarwaljpr@gmail.com>
2024-05-26 15:11:29 +0530

    chore: add final design changes

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react"
 import { env } from "@/env.mjs"
 import { FiGithub, FiLinkedin, FiRss } from "react-icons/fi"
-import { RiTwitterXFill } from "react-icons/ri"
+import { RiAtLine, RiTwitterXFill } from "react-icons/ri"
+import { TfiHeart } from "react-icons/tfi"
 
 import { AnalogClock } from "./ui/clock/clock"
 import Link from "./ui/link"
 
 const Socials = [
+  {
+    name: "Email",
+    url: "mailto:yashagarwaljpr+blog@gmail.com",
+    icon: RiAtLine,
+  },
   {
     name: "Github",
     url: "https://github.com/yashhere",
@@ -47,7 +53,7 @@ export const Footer: FC = () => {
       <div className="mx-auto max-w-2xl space-y-8 p-4 md:px-0">
         <div className="flex flex-row flex-wrap items-start justify-between md:flex-row">
           <section className="flex flex-col  ">
-            <h1 className="mb-4 text-lg font-bold">Follow Me!</h1>
+            <h1 className="mb-4 text-lg font-bold">Get in touch</h1>
             <div className="space-y-2">
               {Socials.map((s) => {
                 return (
@@ -56,9 +62,9 @@ export const Footer: FC = () => {
                     key={s.name}
                     aria-label={s.name}
                     noExternalLinkIcon
-                    className="flex flex-row items-center gap-4 text-text/60 md:gap-2"
+                    className="flex flex-row items-center gap-4 text-text/60 hover:text-text hover:no-underline md:gap-2"
                   >
-                    <s.icon className="size-5 text-text/60 hover:text-text" />
+                    <s.icon className="size-5" />
                     <h1 className="text-lg">{s.name}</h1>
                   </Link>
                 )
@@ -66,7 +72,7 @@ export const Footer: FC = () => {
             </div>
           </section>
           <section className="flex w-1/3 flex-col md:w-auto ">
-            <h1 className="mb-4 text-lg font-bold">Browse more</h1>
+            <h1 className="mb-4 text-lg font-bold">Explore</h1>
             <div className="flex flex-col space-y-2">
               {Object.entries(navItems).map(([path, { name }]) => {
                 return (
@@ -89,7 +95,7 @@ export const Footer: FC = () => {
                   href={`${env.NEXT_PUBLIC_APP_URL}/atom.xml`}
                   aria-label="rss-icon"
                   noExternalLinkIcon
-                  className="flex h-10 flex-row items-center gap-2 rounded-3xl border border-gray-400/50 bg-background px-4 py-1 text-text/60 hover:border-gray-400 hover:text-text"
+                  className="flex h-10 flex-row items-center gap-2 rounded-3xl border border-gray-400/50 bg-background px-4 py-1 text-text/60 shadow-md hover:border-gray-400 hover:text-text"
                 >
                   <FiRss className="size-6" />
                   <span className="text-lg">Subscribe via RSS Feed</span>
@@ -99,7 +105,7 @@ export const Footer: FC = () => {
             <section className="">
               <h1 className="mb-4 text-lg font-bold">Get email updates</h1>
               <div className="mb-6 flex flex-col">
-                <form className="shadow-border flex h-10 items-center justify-between gap-2 overflow-hidden rounded-3xl border-[1px] border-gray-400/50 bg-background focus-within:border-gray-400 focus-within:outline-none">
+                <form className="shadow-border flex h-10 items-center justify-between gap-2 overflow-hidden rounded-3xl border-[1px] border-gray-400/50 bg-gray-100/50 focus-within:border-gray-400 focus-within:outline-none">
                   <input
                     type="text"
                     name="name"
@@ -123,13 +129,19 @@ export const Footer: FC = () => {
         <div className="flex flex-col items-start justify-between md:flex-row"></div>
       </div>
 
-      {/*
-      1. Last Updated on
-      2. Made with love in Jaipur
-      3. Analog Clock
-      4. copyright
-
-      */}
+      <div className="mx-auto max-w-2xl space-y-8 p-4 text-lg md:px-0">
+        <div className="flex w-full flex-row justify-between">
+          <div className="">Be nice ツ</div>
+          <div className="flex flex-row items-center gap-2">
+            <div>©</div>
+            <div>2016</div>
+            <div>-</div>
+            <div className="flex flex-row">
+              <AnalogClock />
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
