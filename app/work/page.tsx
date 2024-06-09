5244c1bf3e85a34694c37dcd5c204a92e64313bc
Yash Agarwal <yashagarwaljpr@gmail.com>
2024-06-09 18:39:54 +0530

    chore: remove prose-article class

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -21,7 +21,7 @@ const Page: FC = () => {
   return (
     <>
       <SectionTitle data={null} title="work" />
-      <section className="prose prose-lg prose-article leading-7">
+      <section className="prose prose-lg leading-7">
         <p>
           I am a enthusiastic software developer with a knack for backend
           development. I&apos;ve got my hands dirty in Python, Golang, and C++.
