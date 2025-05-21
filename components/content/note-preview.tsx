3ef33457e46c6fb64a09b2ffbe622af8b66b7f69
Yash Agarwal <13733968+yashhere@users.noreply.github.com>
2025-05-21 20:29:52 +0530

    feat: note listing redesign

--- components/content/note-preview.tsx
+++ components/content/note-preview.tsx
@@ -14,15 +14,15 @@ export const NotePreview = ({
     <>
       <Link
         href={`/notes/${note.note.slug}`}
-        className="-mx-3 flex flex-col justify-between rounded-md px-3 leading-6 hover:bg-muted sm:py-2 md:flex-row md:items-center"
+        className="flex flex-col justify-between leading-6 rounded-md px-4 md:gap-3 py-2 transition-colors select-none hover:bg-muted md:flex-row md:items-center"
         noUnderline
       >
         <div className="flex flex-col">
-          <span className="mb-[2px] font-sans text-base text-foreground/90">
+          <span className="mb-1 font-sans text-base text-foreground/90">
             {note.note.title}
           </span>
           {homePage && note.note.description && (
-            <span className="mb-[2px] text-sm text-muted-foreground">
+            <span className="mb-1 text-sm text-muted-foreground">
               {note.note.description}
             </span>
           )}
@@ -30,10 +30,17 @@ export const NotePreview = ({
         <span
           className={cn(
             homePage ? "hidden" : "",
+            "h-px flex-1 bg-muted-foreground/20"
+          )}
+          aria-hidden
+        />
+        <span
+          className={cn(
+            homePage ? "hidden" : "",
             "text-sm text-muted-foreground sm:text-base"
           )}
         >
-          {moment(note.note.createdOn).format("MMM DD, YYYY")}
+          {moment(note.note.createdOn).format("MM.YY")}
         </span>
       </Link>
     </>
