a2716edcf654543a298210f09c2add116f0964f4
Yash Agarwal <13733968+yashhere@users.noreply.github.com>
2025-05-24 18:14:23 +0530

    feat: add last visitor functionality

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,9 +1,5 @@
-"use client"
-
-import { FC } from "react"
-import { usePathname } from "next/navigation"
+import { FC, Suspense } from "react"
 import { env } from "@/env.mjs"
-import { cn } from "@/lib/utils"
 import {
   AtIcon,
   GithubLogoIcon,
@@ -12,9 +8,11 @@ import {
   XLogoIcon,
 } from "@phosphor-icons/react/dist/ssr"
 
+import { LastVisitor } from "../interactive/last-visitor"
 import Newsletter from "../interactive/newsletter"
 import { AnalogClock } from "../ui/clock/clock"
 import Link from "../ui/link"
+import { FooterNavigation } from "./footer-nav"
 
 const Socials = [
   {
@@ -44,30 +42,7 @@ const Socials = [
   },
 ]
 
-// same is present in navigation.tsx as well.
-const navItems = {
-  "/notes": {
-    name: "notes",
-  },
-  "/work": {
-    name: "work",
-  },
-  "/colophon": {
-    name: "colophon",
-  },
-  // "/changelog": {
-  //   name: "changelog",
-  // },
-}
-
 export const Footer: FC = () => {
-  let pathname = usePathname() || "/"
-
-  // a note should also show `notes` as active tab
-  if (pathname.includes("/notes/")) {
-    pathname = "/notes"
-  }
-
   return (
     <footer className="mt-auto w-full space-y-4 bg-muted/40">
       <hr className="border-t-1 border-border" />
@@ -96,29 +71,7 @@ export const Footer: FC = () => {
               })}
             </div>
           </section>
-          <section className="flex w-1/3 flex-col md:w-auto">
-            <h1 className="mb-4 text-base font-medium">Explore</h1>
-            <div className="flex flex-col space-y-2">
-              {Object.entries(navItems).map(([path, { name }]) => {
-                const isActive = path === pathname
-                return (
-                  <Link
-                    key={path}
-                    href={path}
-                    className={cn(
-                      "text-base capitalize text-foreground hover:text-foreground hover:no-underline",
-                      {
-                        "text-foreground/60": !isActive,
-                      }
-                    )}
-                    variant="nav"
-                  >
-                    {name}
-                  </Link>
-                )
-              })}
-            </div>
-          </section>
+          <FooterNavigation />
           <div className="mt-8 flex flex-col md:mt-0">
             <section>
               <h1 className="mb-4 text-base font-medium">Get email updates</h1>
@@ -126,6 +79,9 @@ export const Footer: FC = () => {
                 <Newsletter />
               </div>
             </section>
+            <Suspense fallback={null}>
+              <LastVisitor />
+            </Suspense>
           </div>
         </div>
         <div className="flex flex-col items-start justify-between md:flex-row"></div>
@@ -134,14 +90,16 @@ export const Footer: FC = () => {
       <div className="mx-auto max-w-2xl space-y-8 p-4 text-base md:px-0">
         <div className="flex w-full flex-row justify-between">
           <div>Be nice ツ</div>
-          <div className="flex flex-row items-center gap-2">
-            <div>©</div>
-            <div>2016</div>
-            <div>-</div>
-            <div className="flex flex-row">
-              <AnalogClock />
+          <Suspense fallback={null}>
+            <div className="flex flex-row items-center gap-2">
+              <div>©</div>
+              <div>2016</div>
+              <div>-</div>
+              <div className="flex flex-row">
+                <AnalogClock />
+              </div>
             </div>
-          </div>
+          </Suspense>
         </div>
       </div>
     </footer>
