fbbdf72091627db284413d44dae0e24b08edcc94
Yash Agarwal <13733968+yashhere@users.noreply.github.com>
2025-05-25 21:45:06 +0530

    feat: add animation on main content

---
+++ components/ui/animate-enter.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { domAnimation, LazyMotion, m } from "motion/react"
+
+export default function AnimateEnter({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <LazyMotion features={domAnimation}>
+      <m.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 20 }}
+        className="w-full h-full flex flex-col items-center"
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        {children}
+      </m.div>
+    </LazyMotion>
+  )
+}
