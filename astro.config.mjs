d2004b08fae3a194aa6184f38195cfb718bdaa1f
Yash Agarwal <13733968+yashhere@users.noreply.github.com>
2025-12-13 13:20:28 +0530

    migrate work page
    add rss, sitemap, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yashagarwal.in',
   integrations: [alpinejs(), react(), mdx(), sitemap()],
   adapter: cloudflare(),
   vite: {
