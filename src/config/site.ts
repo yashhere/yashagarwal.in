83fe2aca573b5683e3416226a47d074f1b2d0d9a
Yash Agarwal <13733968+yashhere@users.noreply.github.com>
2025-12-13 12:58:49 +0530

    add home page -- not perfect though

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Yash Agarwal",
+  description: "Personal blog and portfolio of Yash Agarwal, a Frontend Engineer.",
+  url: "https://yashagarwal.in",
+  ogImage: "https://yashagarwal.in/og.jpg",
+  featuredNotes: 5,
+  links: {
+    twitter: "https://twitter.com/yashagarwal",
+    github: "https://github.com/yashhere",
+    linkedin: "https://linkedin.com/in/theyashagarwal",
+  },
+};
