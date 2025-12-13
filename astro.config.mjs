996c3953f5a33a3bc3419f810e54b325ac1b939c
Yash Agarwal <13733968+yashhere@users.noreply.github.com>
2025-12-13 11:39:20 +0530

    initialize astro project

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import alpinejs from '@astrojs/alpinejs';
+
+import react from '@astrojs/react';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [alpinejs(), react(), mdx(), sitemap()],
+  adapter: cloudflare(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
